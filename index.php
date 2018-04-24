0d7c9673d4cbf7c8ffa1248ef482a856c332712d
Tsvetelina <panayotova.tsvetelina@gmail.com>
2018-04-24 20:14:34 +0300

    fixed conf

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+spl_autoload_register(function ($class) {
+       $class = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php' ;
+       require_once __DIR__ . DIRECTORY_SEPARATOR . $class;
+    });
+
+
+session_start();
+\Model\DAO\DAO::init();
+
+$fileNotFound = false;
+//request?target=user&action=register
+//would lead to invoking register method in /Controller/UserController
+$controllerName = isset($_GET['target']) ? $_GET['target'] : 'base';
+$methodName = isset($_GET['action']) ? $_GET['action'] : 'index';
+
+$controllerClassName = '\\Controller\\' . $controllerName . 'Controller';
+
+if (class_exists($controllerClassName))
+    {
+        $contoller = new $controllerClassName();
+        if (method_exists($contoller, $methodName)) {
+        //        if request is not for login or register, check for login
+        //        if(!($controllerName == "user" && $methodName == "login")){
+        //            if(!isset($_SESSION["user"])){
+        //                header("HTTP/1.1 401 Unauthorized");
+        //                die();
+        //            }
+        //        }
+                try{
+                        $contoller->$methodName();
+                    }
+        catch(\PDOException $e){
+            //            header("HTTP/1.1 500");
+            echo $e->getMessage();
+            die();
+        }
+    } else {
+                $fileNotFound = true;
+            }
+} else {
+        $fileNotFound = true;
+    }
+
+
+if ($fileNotFound) {
+        //return header 404
+        echo 'target or action invalid: target = ' . $controllerName . ' and action = ' .$methodName;
+}
