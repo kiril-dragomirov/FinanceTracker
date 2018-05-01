eecda62bbb3a004ef56c01d217b2459894a53595
Tsvetelina <panayotova.tsvetelina@gmail.com>
2018-05-01 17:53:41 +0300

    Created list with user's cryptocurrencies

--- View/assets/js/addCrypto.js
+++ View/assets/js/addCrypto.js
@@ -1,31 +1,29 @@
+function addCrypto() {
+    var str = "";
+    str += "<div class=\"form-group\"><label>Crypto Abbreviation</label><input type=\"text\" class=\"form-control\" id=\"cryptoAbb\"></div>";
 
-        var str = "";
-        str += "<div class=\"form-group\"><label>Type Amount</label><input type=\"text\" class=\"form-control\" id=\"cryptoName\"></div>";
-
-        str += "<div class=\"form-group\"><label>Type Amount</label><input type=\"text\" class=\"form-control\" id=\"cryptoAbb\"></div>";
-
-        str += "<button type=\"submit\"  id=\"but\" class=\"btn btn-primary\" >Add</button>";
-        console.log(str);
-        document.getElementById("addCrypto").innerHTML = str;
+    str += "<button type=\"submit\"  id=\"but\" class=\"btn btn-primary\" >Add</button>";
+    console.log(str);
+    document.getElementById("addCrypto").innerHTML = str;
 
 
+    document.getElementById("but").addEventListener("click", function () {
 
-document.getElementById("but").addEventListener("click",function(){
 
-    var cryptoName = document.getElementById("cryptoName").value;
-    var cryptoAbb = document.getElementById("cryptoAbb").value;
-    var r = new XMLHttpRequest();
-    r.open("post", "../index.php?target=crypto&action=addCryptocurrency" );
-    r.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-    r.onreadystatechange = function (ev) {
-        if(r.readyState === 4 && r.status === 200){
+        var cryptoAbb = document.getElementById("cryptoAbb").value;
+        var r = new XMLHttpRequest();
+        r.open("post", "../index.php?target=crypto&action=addCryptocurrency");
+        r.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        r.onreadystatechange = function (ev) {
+            if (r.readyState === 4 && r.status === 200) {
+                listUserCrypto();
+                console.log(cryptoAbb);
+                console.log(this.responseText);
+                document.getElementById("error").innerHTML = this.responseText;
 
-            console.log(cryptoAbb);
-            console.log(cryptoName);
-            console.log(this.responseText);
-            document.getElementById("error").innerHTML = this.responseText;
-
+            }
         }
-    }
-    r.send("cryptoName="+ cryptoName +"&cryptoAbb=" + cryptoAbb );
-});
+        r.send("cryptoAbb=" + cryptoAbb);
+    });
+
+}
---
+++ View/assets/js/listUserCrypto.js
@@ -0,0 +1,45 @@
+function listUserCrypto() {
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("get", "../index.php?target=crypto&action=showUserCrypto");
+    xhr.onreadystatechange = function (ev) {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log(this.responseText);
+            var example = JSON.parse(this.responseText);
+            console.log(example);
+
+            var str = "<table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" class=\"table table-striped table-bordered\">";
+            str += "<tr><th>Name</th>";
+            str += "<th><span class=\"glyphicon glyphicon-usd\"></span> USD (price)</th>";
+            str += "<th colspan=\"2\"><span class=\"glyphicon glyphicon-euro\"></span> EUR (price)</th></tr>";
+
+
+            for (var j = 0; j < example.length; j++) {
+                str += "<tr >";
+                for (var item in example[j]) {
+                    if (item == "USD") {
+                        str += "<td style=\"background-color: #f4f442;\"> <span class=\"glyphicon glyphicon-usd\"></span> ";
+                        str += example[j][item];
+                        str += "</td>";
+                    } else if (item == "EUR") {
+                        str += "<td style=\"background-color: #f4f442;\"> <span class=\"glyphicon glyphicon-euro\"></span> ";
+                        str += example[j][item];
+                        str += "</td>";
+                    } else {
+                        str += "<td style=\"background-color: #f4f442;\">";
+                        str += example[j][item];
+                        str += "</td>";
+                    }
+                }
+                str += "</tr>";
+            }
+            str += "</table>";
+            document.getElementById("listCrypto").innerHTML = str;
+            console.log(str);
+
+        }
+    }
+
+    xhr.send();
+
+}
